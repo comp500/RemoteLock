4f0e9c3c2d0e707160ac95cba4dd10dade725ab5
comp500 <comp500@users.noreply.github.com>
2018-11-06 17:09:36 +0000

    Use syscall rather than Cgo

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
-/*
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0601
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <strsafe.h>
-
-void Lock() {
-	LockWorkStation();
-}
-*/
-import "C"
 import (
 	"fmt"
 	"net/http"
 	"os"
+	"syscall"
 
 	"github.com/gobuffalo/packr"
 )
+
+var user = syscall.NewLazyDLL("user32.dll")
 
 func main() {
 	// Store files for the interface (packed into binary) in the interface folder
@@ -28,7 +17,7 @@ func main() {
 
 	// Serve static files from interface folder
 	http.Handle("/", http.FileServer(box))
-	// Handle websocket endpoints
+	// Handle killswitch endpoint
 	http.HandleFunc("/kill", buttonHandler)
 
 	err := http.ListenAndServe(":3000", nil)
@@ -36,12 +25,16 @@ func main() {
 		fmt.Printf("Error starting interface server: %v\n", err)
 		os.Exit(1)
 	}
+}
 
+func lockScreen() {
+	proc := user.NewProc("LockWorkStation")
+	_, _, _ = proc.Call()
 }
 
 func buttonHandler(w http.ResponseWriter, r *http.Request) {
-	C.Lock();
-	w.Header().Set("Location", "/")
+	lockScreen()
+	w.Header().Set("Location", "/?success")
 	w.WriteHeader(302)
 	fmt.Fprintf(w, "Redirecting")
 }
